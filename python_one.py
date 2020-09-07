edb7724717c524b1887ea8c95b4e2c3ea3259ccb
janefrance <69685490+janefrance@users.noreply.github.com>
2020-09-07 23:10:34 +0100

    Add files via upload
    assignment on python.

---
+++ python_one.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 9900==9900
+True
+>>> 5560-567
+4993
+>>> 5789-657
+5132
+>>> 7885+7647
+15532
+>>> 56768+7748
+64516
+>>> 5665<=5665
+True
+>>> 768>67
+True
+>>> 8585<890
+False
+>>> 67757!=6675
+True
+>>> 
